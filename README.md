fc0d6bdce66cd6d24f8ed49a18061c2c0486e857
Madhuri Chitikela <madhurisravan.chitikela@gmail.com>
2021-05-05 19:58:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Counter application
+
+description: to be written
+
+[View Demo here](https://madhuri-chitikela.github.io/review-carousel/)
+
+<img src="docs/Counter.png" height="500" />
+
+## Tech stack
+
+- HTML
+- CSS
+- Bootstrap
+- Vanilla JavaScript
+
+## Courtesy
+
+[Build 15 JavaScript Projects - Vanilla JavaScript Course](https://www.youtube.com/watch?v=3PHXvlpOkf4)
---
+++ app.js
@@ -0,0 +1,74 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Madhuri",
+        job: "SW-Developer",
+        img: "madhuri.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        id: 2,
+        name: "Vihari",
+        job: "The Boss",
+        img: "vihari.JPG",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        id: 3,
+        name: "Sravan",
+        job: "UX-Designer",
+        img: "sravan.jpg",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+        id: 4,
+        name: "Srinu",
+        job: "Web-Developer",
+        img: "srinu.JPG",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    }
+];
+
+const author = document.getElementById("author");
+const img = document.getElementById("person-img");
+const info = document.getElementById("info");
+const job = document.getElementById("job");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nxtBtn = document.querySelector(".nxt-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+let currentItem = 0;
+
+window.addEventListener("DOMContentLoaded", function () {
+    showPerson();
+})
+
+function showPerson() {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+nxtBtn.addEventListener("click", function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson();
+})
+
+prevBtn.addEventListener("click", function () {
+    currentItem--;
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson();
+})
+
+randomBtn.addEventListener("click", function () {
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson();
+})
